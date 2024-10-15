34d7b4727ac743b19758fd6c670efb561840f32e
trankien214 <kientse171441@fpt.edu.vn>
2024-10-15 08:32:05 +0700

    Product Ongoing

---
+++ HELLWASHER/BusinessObject/IService/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObject.Model.Request.CreateRequest;
+using BusinessObject.Model.Request.UpdateRequest.Entity;
+using BusinessObject.Model.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.IService
+{
+    public interface IProductService
+    {
+        Task<ServiceResponse<PaginationModel<ResponseProductDTO>>> GetAllProducts(int page, int pageSize, string? search, string sort);
+        Task<ServiceResponse<ResponseProductDTO>> GetProductById(int id);
+        Task<ServiceResponse<ResponseProductDTO>> CreateProduct(CreateProductDTO userDTO);
+        Task<ServiceResponse<ResponseProductDTO>> UpdateProduct(int id, UpdateProductDTO userDTO);
+        Task<ServiceResponse<bool>> DeleteProduct(int id);
+        Task<ServiceResponse<ResponseProductDTO>> ChangeProductStatus(int id, string status);
+    }
+}
