3f5dc43a9879f214f9fc1fb3123729f5e17a5fed
KhoaVD12 <duykhoa2kk@gmail.com>
2024-11-28 17:36:45 +0700

    (From another branch) Add Entity for Payos

--- HELLWASHER/DataAccess/Entity/Payment.cs
+++ HELLWASHER/DataAccess/Entity/Payment.cs
@@ -14,6 +14,7 @@ namespace DataAccess.Entity
         public decimal Amount { get; set; }
         public string Description { get; set; }
         public int OrderId {  get; set; }
+        public long OrderCode { get; set; }
         public string Currency { get; set; }
         public string PaymentLinkId { get; set; }
         public string Status { get; set; }
--- HELLWASHER/DataAccess/WashShopContext.cs
+++ HELLWASHER/DataAccess/WashShopContext.cs
@@ -21,72 +21,15 @@ namespace DataAccess
         public DbSet<User> Users { get; set; }
         public DbSet<Payment> Payments { get; set; }
         public DbSet<Feedback> Feedbacks { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<PaymentLinkInformation> PaymentLinkInformation { get; set; }
 
-
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-
-
-        //    /*// 2. User -> Faq (One-to-Many)
-        //    modelBuilder.Entity<Faq>()
-        //        .HasOne(f => f.User)
-        //        .WithMany(u => u.Faqs)
-        //        .HasForeignKey(f => f.UserID);*/
-
-
-
-
-
-        //    // 5. Cart -> CartItem (One-to-Many)
-        //    modelBuilder.Entity<CartItem>()
-        //        .HasOne(ci => ci.Cart)
-        //        .WithMany(c => c.CartItems)
-        //        .HasForeignKey(ci => ci.CartId);
-
-        //    // 6. Service -> CartItem (One-to-Many)
-        //    modelBuilder.Entity<CartItem>()
-        //        .HasOne(ci => ci.Service)
-        //        .WithMany(s => s.CartItems)
-        //        .HasForeignKey(ci => ci.ServiceId);
-
-
-
-        //    // 10. Category -> Service (One-to-Many)
-        //    modelBuilder.Entity<WashService>()
-        //        .HasOne(s => s.Category)
-        //        .WithMany(c => c.Services)
-        //        .HasForeignKey(s => s.CategoryId);
-        //    // Service -> CartItem (One-to-Many)
-        //    modelBuilder.Entity<CartItem>()
-        //        .HasOne(c => c.Service)
-        //        .WithMany(s => s.CartItems)
-        //        .HasForeignKey(c => c.ServiceId);
-        //    // 13. ServiceStatus -> Service (One-to-Many)
-        //    modelBuilder.Entity<WashService>()
-        //        .HasOne(s => s.ServiceStatus)
-        //        .WithMany(ss => ss.Services)
-        //        .HasForeignKey(s => s.ServiceStatusId);
-
-        //    // 14. OrderStatus -> Order (One-to-Many)
-        //    modelBuilder.Entity<Order>()
-        //        .HasOne(o => o.OrderStatus)
-        //        .WithMany(os => os.Orders)
-        //        .HasForeignKey(o => o.OrderStatusId);
-
-        //    // 15. WashStatus -> Order (One-to-Many)
-        //    modelBuilder.Entity<Order>()
-        //        .HasOne(o => o.WashStatus)
-        //        .WithMany(ws => ws.Orders)
-        //        .HasForeignKey(o => o.WashStatusId);
-
-        //    modelBuilder.Entity<Order>()
-        //        .HasOne(o=>o.Cart)
-        //        .WithOne(c => c.Order)
-        //        .HasForeignKey<Order>(c=>c.CartId);
-        //    // Setting up relationships for the entities
-        //    // Configure additional constraints, indexes, etc., if necessary
-        //    base.OnModelCreating(modelBuilder);
-        //}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PaymentLinkInformation>()
+                .HasKey(k => k.PaymentLinkInformationId);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 
 }
